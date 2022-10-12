33809d53deb2bfb20f6b457b5ca2ebadc2361d11
sara-montasser <saramontasser4@gmail.com>
2022-10-12 14:50:01 +0200

    chore: Add .gitignore

--- musicplatform/musicplatform/settings.py
+++ musicplatform/musicplatform/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xn1od0e_=smbvab1w*33soz(txlkzfiuuzf87g3gxc6b8b@6%!'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
