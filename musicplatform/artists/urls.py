4d04c6185f561a4666d0ffd98364033f7889cb55
sara-montasser <saramontasser4@gmail.com>
2022-10-31 01:28:39 +0200

    feat: Add artist data display page

--- musicplatform/artists/urls.py
+++ musicplatform/artists/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.get_artist_form,name="artist"),
+    path('', views.get_all_artist,name="data"),
 ]
--- musicplatform/artists/views.py
+++ musicplatform/artists/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from artists.models import Artist
 from .form import new_artist
 
 def get_artist_form(request):
@@ -11,3 +11,6 @@ def get_artist_form(request):
     else:
         form = new_artist()
     return render(request, 'artist\\newArtist.html', {'form': form})
+
+def get_all_artist(request):
+    return render(request, 'artist\\displayArtists.html', {'Artist': Artist.objects.all()})
