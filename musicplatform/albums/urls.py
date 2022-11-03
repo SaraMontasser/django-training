40094947ee7800c4e96c83b2e8a17e2b969b448b
sara-montasser <saramontasser4@gmail.com>
2022-11-03 02:24:00 +0200

    feat: change the album view to class based view

--- musicplatform/albums/urls.py
+++ musicplatform/albums/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.get_album_form,name="album"),
+    path('create/', views.get_album_form.as_view(),name="album"),
 ]
--- musicplatform/albums/views.py
+++ musicplatform/albums/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .form import AlbumForm
 
-def get_album_form(request):
-    if request.method == 'POST':
-        form = AlbumForm(request.POST)
+class get_album_form(View):
+    form_class = AlbumForm
+    template_name = 'album\\newAlbum.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
-    else:
-        form = AlbumForm()
-    return render(request, 'album\\newAlbum.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
