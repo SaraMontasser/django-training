204c844b26e42a20235fecaf8248f6207fdc3ae9
sara-montasser <saramontasser4@gmail.com>
2022-11-13 03:30:51 +0200

    fix: Fix the permissions for the users view

--- musicplatform/users/views.py
+++ musicplatform/users/views.py
@@ -5,10 +5,12 @@ from rest_framework.views import APIView
 from users.serializers import UserSerializer
 from users.models import User
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class GetUser (APIView):
     authentication_classes = [TokenAuthentication] 
+    permission_classes = [IsAuthenticated]
     def get(self, request, pk):
         user = get_object_or_404(User, id=pk)
         serializer = UserSerializer(user)
